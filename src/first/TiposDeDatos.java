558af670ab30c5102dad66ede50ce1cb6176a1b7
Rafaelowsky <319055539@cuautitlan.unam.mx>
2024-09-12 23:56:22 -0600

    Añadí programa de tipos de dato

---
+++ src/first/TiposDeDatos.java
@@ -0,0 +1,25 @@
+package first;
+
+public class TiposDeDatos {
+	public static void main (String[] args) {
+		int numeroEntero;
+		double numeroDecimal;
+		char letra;
+		String nombre, apellido;
+		
+		// Estos son los tipos de datos que existen en Java
+		numeroEntero = 10;
+		numeroDecimal = 10.5;
+		letra = 'a';
+		nombre = "Rafael";
+		apellido = "Zavala";
+		
+		// Esta parte del código imprime los valores
+		System.out.println(numeroEntero);
+		System.out.println(numeroDecimal);
+		System.out.println(letra);
+		System.out.println(nombre);
+		System.out.println(apellido);
+		
+	}
+}
