45aca9ef42e8a37d9ec8987cd94f2ac7939b094a
Rafaelowsky <319055539@cuautitlan.unam.mx>
2024-09-14 00:39:43 -0600

    Hice el TODO

--- src/first/Incrementadores.java
+++ src/first/Incrementadores.java
@@ -1,15 +1,31 @@
 // Programa para diferencias los incrementadores antes y después
 package first;
 
-// TODO: comentar el codigo y hacer las mismas operaciones pero con el decrementador --
 public class Incrementadores {
 	public static void main(String[] args) {
-		int y = 4;
-		int z = ++y * 2;
-		System.out.println(z);
 		
-		y = 4;
-		z = y++ * 2;
-		System.out.println(z);
+		int y = 4;
+		int z = ++y * 2; // En esta operacion el incrementador al estar antes, el aumento se realiza antes
+	
+		System.out.println(z); // 10
+		
+		int a = 4;
+		int b = a++ * 2; // Al estar después se hace el aumento a la variable al final de la operacion
+						 // sin tomar en cuenta su valor incrementado en la operación.
+		
+		System.out.println(a); // 5 
+		System.out.println(b); // 8
+		
+		
+		int y1 = 4;
+		int z1 = --y1 * 2;
+	
+		System.out.println(z1); // 6
+		
+		int a1 = 4;
+		int b1 = a1 -- * 2;
+		
+		System.out.println(a1); // 3 
+		System.out.println(b1); // 8
 	}
 }
