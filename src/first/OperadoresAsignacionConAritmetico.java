2840c34a9bc80ea7634ed4fd879adc352cf99642
Rafaelowsky <319055539@cuautitlan.unam.mx>
2024-09-13 23:59:40 -0600

    cree el programa OperadoresAsignacionConAritmetico.java

---
+++ src/first/OperadoresAsignacionConAritmetico.java
@@ -0,0 +1,23 @@
+package first;
+//TODO: Agregar los valores que aparecen en consola para cada uno de las operaciones
+public class OperadoresAsignacionConAritmetico {
+	public static void main(String[] args) {
+		int a = 10;
+		
+		a += 5;
+		System.out.println(a);
+		a -= 3;
+		System.out.println(a);
+		a *= 7;
+		System.out.println(a);
+		a /= 5;
+		System.out.println(a);
+		a %= 3;
+		System.out.println(a);
+		a ++;
+		System.out.println(a);
+		a --;
+		System.out.println(a);
+	}
+
+}
