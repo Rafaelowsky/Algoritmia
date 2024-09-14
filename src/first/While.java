087b8b4d6f7db65494280038967f4581b38ca51f
Rafaelowsky <319055539@cuautitlan.unam.mx>
2024-09-14 17:15:04 -0600

    Cree el programa while.java

---
+++ src/first/While.java
@@ -0,0 +1,12 @@
+// Este programa muestra como funciona la iteracion while
+package first;
+
+public class While {
+	public static void main(String[] args) {
+		int i = 0;
+		while (i <= 10) { // Este es un ejemplo de como se utiliza el ciclo while
+			System.out.println(i);
+			i++;
+		}
+	}
+}
