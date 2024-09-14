0e05b642335c7550b9b722190ca18bfe5ad24be1
Rafaelowsky <319055539@cuautitlan.unam.mx>
2024-09-14 00:24:07 -0600

    Hice el TODO

--- src/first/EjemplosOperaciones.java
+++ src/first/EjemplosOperaciones.java
@@ -1,26 +1,25 @@
 /* Este programa muestra ejemplos de como utilizar las operaciones dentro de JAVA */
 package first;
 
-// TODO: Agregar comentarios sobre que hace cada linea
 public class EjemplosOperaciones {
 	public static void main(String[] args) {
 		
 		int a = 3;
 		int b = 5;
 		
-		int sum = a + b;
-		int rest = a - b;
-		int mult = a * b;
-		int div = a / b;
-		double div1 = a / b;
-		double residuo = a % b; 
+		int sum = a + b; // operador para sumar
+		int rest = a - b; // operador para restar
+		int mult = a * b; // operador para multiplicar
+		int div = a / b; // operador para dividir (division de enteros)
+		double div1 = a / b; //operador para dividir (division de con numeros decimales)
+		double residuo = a % b; // operador para obtener el residuo de una division
 		
 		System.out.println("La suma de " + a + " y " + b + " es: " + sum);
 		System.out.println("La resta de " + a + " y " + b + " es: " + rest);
 		System.out.println("La multiplicación de " + a + " y " + b + " es: " + mult);
 		System.out.println("La division de " + a + " y " + b + " es: " + div);
 		System.out.println("La division de " + a + " y " + b + " es: " + div1);
-		System.out.println("La residuo de divir " + a + " y " + b + " es: " + residuo);
+		System.out.println("La residuo de dividir " + a + " y " + b + " es: " + residuo);
 
 	}
 }
