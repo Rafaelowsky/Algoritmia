dd269d8e8a191b5f900a01a595f6f6347ce8f79f
Rafaelowsky <319055539@cuautitlan.unam.mx>
2024-09-14 16:44:59 -0600

    Cree el programa MostrarDatosEnPantalla

---
+++ src/first/MostrarDatosEnPantalla.java
@@ -0,0 +1,20 @@
+/* 
+ * Este programa muestra como son mostrados los datos en pantalla utilizando
+ * diferentes métodos como print, printf, println
+ *
+ */
+package first;
+
+public class MostrarDatosEnPantalla {
+	public static void main(String[] args) {
+		int valor = 1492;
+		String nombre = "Cristobal Colon";
+		double precisionGrabado = 0.000001;
+		System.out.print("a"); // Este print es para poder imprimir en pantalla pero sin el salto de linea 
+		System.out.println("En " + valor + " fue descubierto Ámerica por " + nombre); // Aqui son los valores concatenados
+		System.out.printf("En %d fue descubierto Ámerica por %s%n", valor, nombre); // Aqui es una forma de imprimir los datos pero con printf
+		System.out.printf("El valor %d equivale a %x en base 16%n", valor, valor);
+		System.out.printf("El valor de Pi es %f%n", Math.PI);
+		System.out.printf("La precision de grabado de los procesadores es %e m%n", precisionGrabado);
+	}
+}
