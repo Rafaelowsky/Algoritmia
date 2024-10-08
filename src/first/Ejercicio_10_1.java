1a771bb738e27ace355be9e7ab0cb5527c872c4b
Rafaelowsky <319055539@cuautitlan.unam.mx>
2024-10-08 15:04:48 -0600

    Agregue el programa Ejercicio_10_1.java

---
+++ src/first/Ejercicio_10_1.java
@@ -0,0 +1,14 @@
+package first;
+
+public class Ejercicio_10_1 {
+	public static void main(String[] args) {
+		int a, b;
+		a = 3; // a = 3
+		System.out.println("a = " + a); 
+		b = a + 5; // b = 8
+//		System.out.println("a = " + a + " y b = " + b);
+		System.out.printf("a = %d y b = %d%n", a, b);
+		a = 7; // a = 7
+		System.out.printf("a = %d y b = %d%n", a, b);
+	}
+}
