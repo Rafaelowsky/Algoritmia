edbbb5a7221612a8c3605f8ffe16722552cced92
Rafaelowsky <319055539@cuautitlan.unam.mx>
2024-10-08 15:25:49 -0600

    Agregue el programa Ejercicio_10_2.java

---
+++ src/first/Ejercicio_10_2.java
@@ -0,0 +1,18 @@
+package first;
+
+public class Ejercicio_10_2 {
+	public static void main(String[] args) {
+		int valor1, valor2;
+		String cadena1;
+		final double real = 48.75;
+		
+		valor1 = 92 % 8; // 4
+		valor2 = valor1 * 2; // 8
+		cadena1 = "Test";
+		
+		System.out.println(cadena1 + " valor2 = " + valor2);
+		// Test valor2 = 8
+		System.out.println(valor1 + " # " + real);
+		// 4 # 48.75
+	}
+}
